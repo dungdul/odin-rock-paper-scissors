bfc2f3ab0a81738ba8672ff55de27ce662bf688e
dungdul <178075100+dungdul@users.noreply.github.com>
2026-01-05 20:59:40 +0900

    Add js to handle gameplay with ui

--- javascript.js
+++ javascript.js
@@ -1,3 +1,12 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const scoreDiv = document.querySelector(".score");
+const messageDiv = document.querySelector(".message");
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const buttons = document.querySelectorAll("button");
+
 function getComputerChoice() {
     n = Math.random()
     if (n < 1 / 3) {
@@ -9,47 +18,31 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    return prompt("Input your choice")
-}
-
-function playGame() {
-
-    // Players' scores
-    let humanScore = 0
-    let computerScore = 0 
-
-    // Function for playing 1 game
-    function playRound(humanChoice, computerChoice) {
-
-        // Make humanChoice case insensitive
-        humanChoice = humanChoice.toLowerCase()
-
-        // Check who winner is, print message, and increment score
-        if (humanChoice === computerChoice) {
-            console.log(`Tie (${humanChoice} : ${computerChoice})`)
-        } else if (
-            humanChoice === "rock" && computerChoice === "paper"
-            || humanChoice === "paper" && computerChoice === "scissors"
-            || humanChoice === "scissors" && computerChoice === "rock"
-        ) {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-            computerScore++
-        } else {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-            humanScore++
-        }
-    }
-
-    // Play game for 5 times
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice())
-    }
-
-    // Declare winner
-    if (humanScore > computerScore) {
-        console.log(`You win! (${humanScore}:${computerScore})`)
+function playRound(humanChoice, computerChoice) {
+    // Check who winner is, display message, and update score
+    if (humanChoice === computerChoice) {
+        messageDiv.textContent = `Tie (${humanChoice} : ${computerChoice})`;
+    } else if (
+        humanChoice === "rock" && computerChoice === "paper"
+        || humanChoice === "paper" && computerChoice === "scissors"
+        || humanChoice === "scissors" && computerChoice === "rock"
+    ) {
+        messageDiv.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+        computerScore++
     } else {
-        console.log(`You lose! (${computerScore}:${humanScore})`)
+        messageDiv.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+        humanScore++
     }
+    scoreDiv.textContent = `You ${humanScore} : ${computerScore} Computer`
+
+    // If the game has ended, display winner and restart button
+//     if (humanScore === 5 || computerScore === 5) {
+
+//     }
 }
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        playRound(event.target.id, getComputerChoice());
+    })
+});
